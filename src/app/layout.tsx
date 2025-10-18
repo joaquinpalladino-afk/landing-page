32605fec5f10095fbdb98ef33c9590459239485f
Joaquin Palladino <joaquinpalladino45@gmail.com>
2025-10-18 14:14:53 -0300

    I have added the Microsoft Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 
@@ -31,6 +32,16 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && <GoogleAnalytics />}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive" // Carga el script después de que la página sea interactiva
+          dangerouslySetInnerHTML={{
+            __html:`    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");`, 
+          }} />
         <div className="min-h-screen bg-slate-700/20 text-white">
         {children}
         </div>
